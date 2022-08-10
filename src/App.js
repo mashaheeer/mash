babf22b3538744ebad3deb4064e68f484a130a75
mashaheeer <sergiopalermo94@gmail.com>
2022-08-10 20:20:05 +0300

    Added data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Influencers from "./components/Influencers/Influencers";
 import InfluencerReview from "./components/InfluencerReview/InfluencerReview";
+
 function App() {
   return (
     <section>
